db3afdd6ee3ed218a3b9704aedbafcc5ec44da39
ElaKarolewska <e.karolewska00@gmail.com>
2023-11-28 15:02:33 +0100

    New Models

---
+++ MagazineApp/MagazineApp/Components/CsvReader/Models/Medicines.cs
@@ -0,0 +1,13 @@
+﻿namespace MagazineApp.Components.CsvReader.Models;
+
+public class Medicines
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Substance { get; set; }
+    public string Packaging { get; set; }
+    public string Power { get; set; }
+    public string Manufacturer { get; set; }
+    public string Country { get; set; }
+
+}
